f95df40690d6263d8d940a2f0c4c2ca2b8250767
JEREMIE BERTRAND <therlys6255@gmail.com>
2019-09-03 17:51:26 -0400

    Move Test in Fox

--- Assets/Scripts/Play/Actor/Animal/Predator/Fox/Fox.cs
+++ Assets/Scripts/Play/Actor/Animal/Predator/Fox/Fox.cs
@@ -1,22 +1,59 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
 
 namespace Game
 {
     public sealed class Fox : Animal, IPredator
     {
+        private bool IsMoving = false;
+
+        private List<Node> nodes = null;
         //TODO : Compléter le comportement du renard.
         private void Update()
         {
+            if (Sensor.SensedObjects.Count == 0)
+            {
+                Mover.MoveTo(PathFinder.FindRandomWalk(Position, 4).ElementAt(0).Position3D);
+            }
             foreach (var sensedObject in Sensor.SensedObjects)
             {
                 var bunny = sensedObject.GetComponent<Bunny>();
-                if (bunny != null)
+                if (!IsMoving && bunny != null)
                 {
+
+                    nodes = PathFinder.FindPath(Position, bunny.Position);
+                    StartCoroutine(Move());
                     // Je suis un @$@!@ de lapin
-                    Mover.MoveTo(sensedObject.transform.position);
-                    bunny.Eat();
+                    //bunny.Eat();
                     break;
                 }
+            }
+        }
+
+        private IEnumerator Move()
+        {
+            IsMoving = true;
+
+            foreach (var node in nodes)
+            {
+                Mover.MoveTo(node.Position3D);
+                yield return new WaitForSeconds(1.0f);
+            }
+
+            IsMoving = false;
+            nodes = null;
+        }
+
+        private void OnDrawGizmos()
+        {
+            if(nodes == null) return;
+
+            for (int i = 1; i < nodes.Count; i++)
+            {
+                GizmosExtensions.DrawArrow(nodes.ElementAt(i-1).Position3D, nodes.ElementAt(i).Position3D, Color.green);
             }
         }
     }
